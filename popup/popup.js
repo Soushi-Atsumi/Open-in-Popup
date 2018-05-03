ac71d625c098ec4bc622cf69eebeed3e25bcfda2
聡志 渥美 <pharaoh.jpn@gmail.com>
2018-05-03 19:24:45 +0900

    Version 1.0

---
+++ popup/popup.js
@@ -0,0 +1,22 @@
+﻿/*
+ * Open in Popup - More useful searching extension than Built-in features.
+ * Copyright (c) 2018 Soushi Atsumi. All rights reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * 
+ * This Source Code Form is "Incompatible With Secondary Licenses", as
+ * defined by the Mozilla Public License, v. 2.0.
+ */
+
+browser.browserAction.getTitle({}).then((toggleTitle) => {
+    if (toggleTitle != '') {
+        window.location = toggleTitle;
+    } else {
+        browser.tabs.create({
+            url: browser.i18n.getMessage("url_index.html")
+        });
+        window.close();
+    }
+});
